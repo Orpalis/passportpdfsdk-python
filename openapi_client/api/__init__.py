a6c0a7ee369c0e6ad380e0a42d83b4eda3d0b6fa
Bouzigouloum <e.carrere@orpalis.com>
2020-04-10 17:40:41 +0200

    Initial commit

---
+++ openapi_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.config_api import ConfigApi
+from openapi_client.api.docu_vieware_api import DocuViewareApi
+from openapi_client.api.document_api import DocumentApi
+from openapi_client.api.image_api import ImageApi
+from openapi_client.api.pdf_api import PDFApi
+from openapi_client.api.passport_manager_api import PassportManagerApi
+from openapi_client.api.passport_pdf_application_manager_api import PassportPDFApplicationManagerApi
